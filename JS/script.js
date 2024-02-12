72333e9c68573a13b365cacbdbb3a61a116c444f
kolesnikov-yuri <knyaz_vetrov@mail.ru>
2024-02-12 21:48:31 +0400

    hover fix

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,81 @@ window.addEventListener('DOMContentLoaded', () => {
     const btns = document.querySelectorAll('.button');
     let index;
 
+//-------------------------------
+
+    const   zero  = 0,
+            one   = 1,
+            two   = 2,
+            three = 3,
+            four  = 4,
+            five  = 5,
+            six   = 6,
+            seven = 7,
+            eight = 8,
+            nine  = 9;
+
+    const clusterNum = {
+        a: zero,
+        b: one,
+        c: two,
+        d: three,
+        e: four,
+        f: five,
+        g: six,
+        h: seven,
+        i: eight,
+        j: nine
+    };
+
+    
+
+    function aaaBbb() {
+        cluster[0].innerHTML = `
+        <div class="cluster_elements">
+            <div class="row row_elements row_elements_1">
+                <div class="element element_1">
+                    <p class="element_value element_value_1">${clusterNum.b}</p>
+                </div>
+                <div class="element element_2">
+                    <p class="element_value element_value_2">${clusterNum.c}</p>
+                </div>
+                <div class="element element_3">
+                    <p class="element_value element_value_3">${clusterNum.d}</p>
+                </div>
+            </div>
+            <div class="row row_elements row_elements_2">
+                <div class="element element_4">
+                    <p class="element_value element_value_4">${clusterNum.e}</p>
+                </div>
+                <div class="element element_5">
+                    <p class="element_value element_value_5">${clusterNum.f}</p>
+                </div>
+                <div class="element element_6">
+                    <p class="element_value element_value_6">${clusterNum.g}</p>
+                </div>
+            </div>
+            <div class="row row_elements row_elements_3">
+                <div class="element element_7">
+                    <p class="element_value element_value_7">${clusterNum.h}</p>
+                </div>
+                <div class="element element_8">
+                    <p class="element_value element_value_8">${clusterNum.i}</p>
+                </div>
+                <div class="element element_9">
+                    <p class="element_value element_value_9">${clusterNum.j}</p>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+
+    aaaBbb();
+
+
+
+
+            
+
     cluster.forEach((clr, i) => {
         clr.addEventListener('click', () => {
             index = i;
