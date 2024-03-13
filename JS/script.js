12c795b5b0ddaf7cae28c80e2301a22d012b02bc
kolesnikov-yuri <knyaz_vetrov@mail.ru>
2024-03-13 16:36:48 +0400

    inner

--- JS/script.js
+++ JS/script.js
@@ -1,41 +1,48 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    const numContent = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//-------------Inner HTML------------------------
+
+    const main = document.querySelector('.main');
+    main.innerHTML = `
+    <div class ="field">
+        <div class ="row row_sectors_s"></div>
+        <div class ="row row_sectors_s"></div>
+        <div class ="row row_sectors_s"></div>
+    </div>
+    <div class ="buttons"></div>
+    <div class="button_start">start</div>
+    `;
+
     const btnsParent = document.querySelector('.buttons');
-    const cluster = document.querySelectorAll('.cluster');
-    const btns = document.querySelectorAll('.button');
-    let index;
+    const rowSectors = document.querySelectorAll('.row_sectors_s');
+    rowSectors.forEach((rowSector) => {
+        rowSector.innerHTML =  `
+            <div class ="sector"></div>
+            <div class ="sector"></div>
+            <div class ="sector"></div>
+        `;
+    }); 
 
-//-------------------------------
+    const sectors = document.querySelectorAll('.sector');
+    sectors.forEach((sector) => {
+        sector.innerHTML =`
+            <div class ="row row_cluster"></div>
+            <div class ="row row_cluster"></div>
+            <div class ="row row_cluster"></div>
+        `;
+    });
 
-    const   zero  = 0,
-            one   = 1,
-            two   = 2,
-            three = 3,
-            four  = 4,
-            five  = 5,
-            six   = 6,
-            seven = 7,
-            eight = 8,
-            nine  = 9;
+    const rowClusters = document.querySelectorAll('.row_cluster');
+    rowClusters.forEach((rowCluster) => {
+        rowCluster.innerHTML = `
+            <div class ="cluster"></div>
+            <div class ="cluster"></div>
+            <div class ="cluster"></div>
+        `;
+    });
 
-    const clusterNum = {
-        a: zero,
-        b: one,
-        c: two,
-        d: three,
-        e: four,
-        f: five,
-        g: six,
-        h: seven,
-        i: eight,
-        j: nine
-    };
-
-    
-
-    function aaaBbb() {
-        cluster[0].innerHTML = `
+    function innerHtmlCluster(i) {
+        cluster[i].innerHTML = `
         <div class="cluster_elements">
             <div class="row row_elements row_elements_1">
                 <div class="element element_1">
@@ -73,13 +80,55 @@ window.addEventListener('DOMContentLoaded', () => {
         </div>
         `;
     }
+    btnsParent.innerHTML = `
+        <div class ="button">0</div>
+        <div class ="button">1</div>
+        <div class ="button">2</div>
+        <div class ="button">3</div>
+        <div class ="button">4</div>
+        <div class ="button">5</div>
+        <div class ="button">6</div>
+        <div class ="button">7</div>
+        <div class ="button">8</div>
+        <div class ="button">9</div>
+    `;
 
-    aaaBbb();
+    // let a;
+    // for (a = 0; a < 10; a++) {
+    //     btnsParent.innerHTML = `<div class ="button button_${a}">${a}</div>`;
+    // }
+    
 
+//-------------Inner HTML end------------------------
+    const numContent = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];   
+    const cluster = document.querySelectorAll('.cluster');
+    const btns = document.querySelectorAll('.button');
+    let index;   
+//-------------------------------
 
+    const   zero  = 0,
+            one   = 1,
+            two   = 2,
+            three = 3,
+            four  = 4,
+            five  = 5,
+            six   = 6,
+            seven = 7,
+            eight = 8,
+            nine  = 9;
 
-
-            
+    const clusterNum = {
+        a: zero,
+        b: one,
+        c: two,
+        d: three,
+        e: four,
+        f: five,
+        g: six,
+        h: seven,
+        i: eight,
+        j: nine
+    };
 
     cluster.forEach((clr, i) => {
         clr.addEventListener('click', () => {
@@ -87,7 +136,7 @@ window.addEventListener('DOMContentLoaded', () => {
             reverClusterBg();
             pourClusterBg(i);
         });
-       
+        innerHtmlCluster(i);
     });
 
     function reverClusterBg() {
